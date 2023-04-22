cb803564d1e0ef1abb97bc202cf304cca14520f5
bismuthsalamander <cliff@cliffsmith.org>
2023-04-21 21:59:29 -0500

    Fixed interface to not require reference to stopwatch.Watch

--- stopwatch.go
+++ stopwatch.go
@@ -1,4 +1,4 @@
-package nurigobe
+package stopwatch
 
 import (
 	"fmt"
@@ -10,19 +10,22 @@ var Watch Stopwatch
 type Stopwatch struct {
 	Buckets      map[string]int64
 	BucketStarts map[string]int64
+	EntryCounts  map[string]int64
 }
 
 func init() {
 	Watch = Stopwatch{}
 	Watch.Buckets = make(map[string]int64)
 	Watch.BucketStarts = make(map[string]int64)
+	Watch.EntryCounts = make(map[string]int64)
 	Watch.Start("")
 }
 
 func (s *Stopwatch) Start(b string) {
-	if _, ok := s.BucketStarts[b] {
+	if _, ok := s.BucketStarts[b]; ok {
 		return
 	}
+	s.EntryCounts[b]++
 	s.BucketStarts[b] = time.Now().UnixNano()
 }
 
@@ -37,12 +40,37 @@ func (s *Stopwatch) Stop(b string) {
 }
 
 func (s *Stopwatch) Results() string {
+	now := time.Now().UnixNano()
 	out := ""
 	for k, v := range s.Buckets {
-		out += fmt.Sprintf("%s: %.4f\n", k, float64(v)/1000000000.0)
+		res := v
+		if start, ok := s.BucketStarts[k]; ok {
+			res += start - now
+		}
+		out += fmt.Sprintf("%s (%d): %.4f\n", k, s.EntryCounts[k], float64(res)/1000000000.0)
 	}
 	s.Stop("")
 	out += fmt.Sprintf("TOTAL: %.4f\n", float64(s.Buckets[""])/1000000000.0)
 	s.Start("")
 	return out
+}
+
+func Start(s string) {
+	Watch.Start(s)
+}
+
+func Stop(s string) {
+	Watch.Stop(s)
+}
+
+func Results() string {
+	return Watch.Results()
+}
+
+func Buckets() map[string]int64 {
+	res := make(map[string]int64)
+	for k, v := range Watch.Buckets {
+		res[k] = v
+	}
+	return res
 }
